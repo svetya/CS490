1110bc6d86d0ff095fbc19936896ca6998fcb8c3
EDG-136 <booboo12keys@gmail.com>
2025-04-22 22:16:02 -0700

    Update VideoThread.py

--- src/view/VideoThread.py
+++ src/view/VideoThread.py
@@ -15,7 +15,6 @@ class VideoThread(QtCore.QThread):
             self.confidence= confidence
             self.classes=classes
             self._run_flag = True
-            self.last_frame = None
 
         def run(self):
             model = Detector(self.model_path, self.confidence)
@@ -26,13 +25,8 @@ class VideoThread(QtCore.QThread):
                 if not ret:
                     break
 
-                results = model.detect(frame, self.confidence, self.classes, stream = True)
-
-                for result in results:
-                    frame = result.plot()  # Annotate the original frame
-
-                    break
-                self.last_frame = frame.copy()
+                frame = model.detect(frame, self.confidence, self.classes)
+                
                 h, w, ch = frame.shape
                 bytes_per_line = ch * w
                 q_image = QtGui.QImage(frame.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
@@ -48,6 +42,3 @@ class VideoThread(QtCore.QThread):
         def stop(self):
             self._run_flag = False
             self.wait()
-
-        def get_current_frame(self):
-            return self.last_frame.copy() if self.last_frame is not None else None
